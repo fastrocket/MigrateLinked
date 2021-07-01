7856ed4c71569c133610cdc6e0902139b0a46982
fastrocket <fastrocket@gmail.com>
2021-06-30 23:25:58 -0600

    Using good .lnk Nuget (Lnk) since previous misread network path

--- MigrateLinked/Program.cs
+++ MigrateLinked/Program.cs
@@ -1,94 +1,112 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
+using Lnk;
 
 namespace MigrateLinked
 {
     class Program
     {
-        static void Main(string[] args)
+        // CHANGE THESE VALUES IF YOU AREN'T USING COMMAND-LINE
+        private static bool isVerbose = true;
+        private static string SRC_DIR_NAME = @"D:\src";
+        private static string DST_DIR_NAME = @"F:\dest";
+
+        private static void Verbose(string str)
         {
-            var destDirName = "c:/tmp/";
-            if (args.Length != 1)
+            if (isVerbose)
             {
-                Console.WriteLine($"MigrateLinked destDir\nINFO: copies from . to destDir and follows links to copy files");
-                return; // comment out to debug using c:/tmp/
-            }
-            else
-            {
-                destDirName = args[0];
-            }
-
-            var destDir = new DirectoryInfo(destDirName);
-            if (!destDir.Exists)
-            {
-                // Try to create destination dir if it doesn't exist
-                Console.WriteLine($"Creating {destDir}");
-                destDir.Create();
-            }
-
-            var dir = new DirectoryInfo(".");
-            var files = dir.GetFiles();
-            foreach (var file in files)
-            {
-                string realName = file.FullName;
-
-                if (Directory.Exists(realName))
-                {
-                    Console.WriteLine($"Skipping directory {realName}");
-                    continue;
-                }
-                //Console.WriteLine($"{file.FullName}");
-                if (file.Extension == ".lnk")
-                {
-                    realName = GetLnkTargetPath(file.FullName);
-                    Console.WriteLine($"!!!! LNK: {realName}");
-                }
-                var destFullName = Path.Combine(destDirName, Path.GetFileName(realName));
-                var destInfo = new FileInfo(destFullName);
-                if (destInfo.Exists)
-                {
-                    Console.WriteLine($"Skipping: {destFullName}");
-                }
-                else
-                {
-                    Console.WriteLine($"Copying {realName} to {destFullName}");
-                    File.Copy(realName, destFullName);
-                }
+                Console.WriteLine(str);
             }
         }
 
-        public static string GetLnkTargetPath(string filepath)
+        static void Main(string[] args)
         {
-            using (var br = new BinaryReader(System.IO.File.OpenRead(filepath)))
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); // Required for Lnk
+
+            var processed = 0;
+            var copied = 0;
+
+          
+            if (args.Length == 2)
             {
-                // skip the first 20 bytes (HeaderSize and LinkCLSID)
-                br.ReadBytes(0x14);
-                // read the LinkFlags structure (4 bytes)
-                uint lflags = br.ReadUInt32();
-                // if the HasLinkTargetIDList bit is set then skip the stored IDList 
-                // structure and header
-                if ((lflags & 0x01) == 1)
-                {
-                    br.ReadBytes(0x34);
-                    var skip = br.ReadUInt16(); // this counts of how far we need to skip ahead
-                    br.ReadBytes(skip);
-                }
-                // get the number of bytes the path contains
-                var length = br.ReadUInt32();
-                // skip 12 bytes (LinkInfoHeaderSize, LinkInfoFlgas, and VolumeIDOffset)
-                br.ReadBytes(0x0C);
-                // Find the location of the LocalBasePath position
-                var lbpos = br.ReadUInt32();
-                // Skip to the path position 
-                // (subtract the length of the read (4 bytes), the length of the skip (12 bytes), and
-                // the length of the lbpos read (4 bytes) from the lbpos)
-                br.ReadBytes((int)lbpos - 0x14);
-                var size = length - lbpos - 0x02;
-                var bytePath = br.ReadBytes((int)size);
-                var path = Encoding.UTF8.GetString(bytePath, 0, bytePath.Length);
-                return path;
+                SRC_DIR_NAME = args[0];
+                DST_DIR_NAME = args[1];
             }
+
+            var destDir = new DirectoryInfo(DST_DIR_NAME);
+            if (!destDir.Exists)
+            {
+                // Try to create destination dir if it doesn't exist
+                Verbose($"Creating {destDir}");
+                destDir.Create();
+            }
+
+            var dir = new DirectoryInfo(SRC_DIR_NAME);
+            var files = dir.GetFiles();
+
+            Verbose($"START: COPY {SRC_DIR_NAME} to {DST_DIR_NAME} : Ignoring any directories & expanding .lnk files");
+
+            foreach (var file in files)
+            {
+                processed++;
+                string relName = file.FullName;
+
+                if (Directory.Exists(relName))
+                {
+                    Verbose($"Skipping directory {relName}");
+                    continue;
+                }
+
+                Verbose($"{file.FullName}");
+
+                if (file.Extension == ".lnk")
+                {
+                    var lnk = Lnk.Lnk.LoadFile(file.FullName);
+                    relName = lnk.RelativePath;
+
+                    if (String.IsNullOrWhiteSpace(relName))
+                    {
+                        Verbose($"SKIPPING invalid .lnk file {file.FullName} is Null or whitespace!!");
+                        continue;
+                    }
+                }
+
+                var destFullName = Path.Combine(DST_DIR_NAME, Path.GetFileName(relName));
+                var destInfo = new FileInfo(destFullName);
+                if (destInfo.Exists)
+                {
+                    Verbose($"EXISTS: Skipping {destFullName}");
+                    continue;
+                }
+                else
+                {
+                    try
+                    {
+                        Console.WriteLine($"TRYING: {relName}");
+                        var combined = Path.Combine(SRC_DIR_NAME, relName);
+                        var origInfo = new FileInfo(combined);
+                        if (origInfo.Exists)
+                        {
+                            Verbose($"COPYING {combined} to {destFullName}");
+                            File.Copy(combined, destFullName);
+                            copied++;
+                        }
+                        else
+                        {
+                            Verbose($"SKIPPING source does not exist: {relName}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Verbose($"SKIPPING {relName} due to exception: {ex.Message}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"FINISHED: processed: {processed} copied: {copied}");
         }
     }
 }
